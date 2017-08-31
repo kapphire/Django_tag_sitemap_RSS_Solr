3060db259497dabc8598acfe6f161692644456c1
kapphire <ken.sapphire87@yahoo.com>
2017-08-31 16:50:06 +0200

    Django1.8 tag RSS Solr

---
+++ blog/migrations/0004_auto_20170826_2106.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0003_comment'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(related_name='comments', to='blog.Post'),
+        ),
+    ]
